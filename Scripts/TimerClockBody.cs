11b4020674cdae889d6ac96a5ed95ed48a39d538
akareactor <akareactor@mail.ru>
2024-02-02 16:12:35 +0300

    Первый коммит, добавлен package.json, пакет был открыт в Юнити.

---
+++ Scripts/TimerClockBody.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Просто счётчик времени, который используется в TimerClock
+// СИНГЛТОН ЕБАНА МАТ
+
+namespace Space.Kulibin.TimerSystem {
+
+	public class TimerClockBody : MonoBehaviour {
+
+		public delegate void TimerClockAction();
+
+		static TimerClockBody instance; 
+		public float duration; // время для отсчёта по таймеру
+		float startTime;
+		public bool startOnStart = true;
+		public bool startOnEnable = false;
+		private bool timerHasStarted = false; // таймер стартовал, перезапускать только по ResetTimer
+		public static float remainder { get { return instance._remainder; }} // запрашивается из клиента
+		float _remainder; // оставшееся время
+		public static event TimerClockAction OnTimerEndAction; // подписка на завершение таймера
+		public UnityEvent onTimerEndAction; // событие по завершении таймера
+
+		public void Awake () {
+			instance = this;
+			instance._remainder = instance.duration;
+		}
+
+		public void ResetTimer () {
+			startTime = Time.time;
+			timerHasStarted = true;
+			instance._remainder = instance.duration;
+		}
+
+		public void ResetTimer (float newDuration) {
+			duration = newDuration;
+			ResetTimer();
+		}
+	
+		public void StopTimer () {
+			timerHasStarted = false;
+		}
+	
+		void Update () {
+			if (timerHasStarted) {
+				if (instance._remainder > 0f) 
+					instance._remainder = instance.duration - (Time.time - startTime);
+				else {
+					timerHasStarted = false;
+					if (OnTimerEndAction != null) OnTimerEndAction();
+					onTimerEndAction.Invoke();
+				}
+			}
+		}
+	
+		void OnEnable () {
+			if (startOnEnable) ResetTimer();
+		}
+	
+		void Start() {
+			if (startOnStart) ResetTimer();
+		}
+
+	}
+
+}
---
+++ Scripts/TimerEvent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+// Самый простой таймер
+namespace Space.Kulibin.TimerSystem {
+
+	public class TimerEvent : MonoBehaviour {
+
+		public float duration; // время для отсчёта по таймеру
+		float startTime;
+		public bool startOnStart = true;
+		public bool startOnEnable = false;
+		private bool timerHasStarted = false; // таймер стартовал, перезапускать только по ResetTimer
+		public float remainder { get { return _remainder; }} // запрашивается из клиента
+		float _remainder; // оставшееся время
+		public UnityEvent onTimerEndAction; // событие по завершении таймера
+
+		public void ResetTimer () {
+			startTime = Time.time;
+			timerHasStarted = true;
+			_remainder = duration;
+		}
+	
+		public void StopTimer () {
+			timerHasStarted = false;
+		}
+	
+		void Update () {
+			if (timerHasStarted) {
+				if (_remainder > 0f) 
+					_remainder = duration - (Time.time - startTime);
+				else {
+					timerHasStarted = false;
+					onTimerEndAction.Invoke();
+				}
+			}
+		}
+	
+		void OnEnable () {
+			if (startOnEnable) ResetTimer();
+		}
+	
+		void Start() {
+			if (startOnStart) ResetTimer();
+		}
+
+	}
+
+}
