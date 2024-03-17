f684aff9fa093cf34ab437f0dadf6936edd422df
akareactor <akareactor@mail.ru>
2024-03-17 18:54:59 +0300

    Форматирование HH:MM:SS

--- Scripts/TimerClockWidget.cs
+++ Scripts/TimerClockWidget.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -14,18 +15,34 @@ namespace KulibinSpace.TimerSystem {
 		public TimerClock timerClock;
 		public TextMeshProUGUI m_textMeshPro;
 		//private TMP_FontAsset m_FontAsset;
-		public string label = "<#0050FF>Time left: </color>{0:2}";
+		public string label = "<#0050FF>Time left: </color>{0:D2}:{1:D2}:{2:D2}";
 		public float refreshRate = 1; // частота обновления дисплея таймера, Гц
+		public bool raw = false; // выводить секунды одним числом, без разбивки на HH:MM:SS 
 		float time;
 
 		void OnEnable() {
 			time = Time.time;
 		}
 
+		void SetFormattedText () {
+			float s = timerClock.value % 1000;
+			if (raw) {
+				m_textMeshPro.SetText(label, s);
+			} else {
+				TimeSpan sec = TimeSpan.FromSeconds(timerClock.value % 1000);
+				m_textMeshPro.SetText(string.Format(label, sec.Hours, sec.Minutes, sec.Seconds));
+			}
+			//m_textMeshPro.SetText(label, TimeSpan.FromSeconds(s).ToString(@"mm\:ss"));
+			//m_textMeshPro.SetText(TimeSpan.FromSeconds(s).ToString(@"mm\:ss"));
+			//string.Format("{0:D2}:{1:D2}:{2:D2}", sec.Hours, sec.Minutes, sec.Seconds
+			//m_textMeshPro.SetText(label, sec.Minutes, sec.Seconds); // label не форматирует нулями, как надо
+			//m_textMeshPro.SetText(string.Format("{0:D2}:{1:D2}:{2:D2}", sec.Hours, sec.Minutes, sec.Seconds));
+		}
+
 		void Update () {
 			if (Time.time - time > 1.0f / refreshRate) {
 				time = Time.time;
-				m_textMeshPro.SetText(label, timerClock.value % 1000);
+				SetFormattedText();
 			}
 		}
 
