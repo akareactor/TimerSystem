3110b68fb0e59ca5a8781379caeb3eff09b6c45b
akareactor <akareactor@mail.ru>
2024-07-18 16:11:00 +0300

    v 1.0.1 Добавлено форматирование виджета обратного отсчёта, копипаста обычного таймера

--- Scripts/TimerCountdownWidget.cs
+++ Scripts/TimerCountdownWidget.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-using UnityEngine.Events;
 using TMPro;
 
 namespace KulibinSpace.TimerSystem {
@@ -15,19 +13,35 @@ namespace KulibinSpace.TimerSystem {
 		//private TMP_FontAsset m_FontAsset;
 		public string label = "<#0050FF>Time left: </color>{0:2}";
 		public float refreshRate = 1; // частота обновления дисплея таймера, Гц
+		public bool raw = false; // выводить секунды одним числом, без разбивки на HH:MM:SS 
 		float time;
 
 		void OnEnable() {
 			time = Time.time;
 		}
 
+		void SetFormattedText (float s) {
+			if (raw) {
+				m_textMeshPro.SetText(label, s);
+			} else {
+				TimeSpan sec = TimeSpan.FromSeconds(s);
+				m_textMeshPro.SetText(string.Format(label, sec.Hours, sec.Minutes, sec.Seconds));
+			}
+			//m_textMeshPro.SetText(label, TimeSpan.FromSeconds(s).ToString(@"mm\:ss"));
+			//m_textMeshPro.SetText(TimeSpan.FromSeconds(s).ToString(@"mm\:ss"));
+			//string.Format("{0:D2}:{1:D2}:{2:D2}", sec.Hours, sec.Minutes, sec.Seconds
+			//m_textMeshPro.SetText(label, sec.Minutes, sec.Seconds); // label не форматирует нулями, как надо
+			//m_textMeshPro.SetText(string.Format("{0:D2}:{1:D2}:{2:D2}", sec.Hours, sec.Minutes, sec.Seconds));
+		}
+
 		void Update () {
 			if (Time.time - time > 1.0f / refreshRate) {
 				time = Time.time;
-				m_textMeshPro.SetText(label, TimerCountdown.remainder % 1000);
+				//m_textMeshPro.SetText(label, TimerCountdown.remainder % 1000);
+                SetFormattedText(TimerCountdown.remainder % 1000);
 			}
 		}
-
+        
 		void Start() {
 			// Add new TextMesh Pro Component
 			if (!m_textMeshPro) m_textMeshPro = gameObject.GetComponent<TextMeshProUGUI>();
